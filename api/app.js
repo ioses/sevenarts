24013968ac72d4e8fc86a3ad68b3e4081bf4526c
ioses <iosesgale@gmail.com>
2018-03-29 10:42:25 +0200

    first commit

---
+++ api/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//Archivo de configuracion general
+
+//Carga el modulo de express. carga de rutas, prot. http
+var express = require('express');
+//Convierte lo que viene por POST,GET.. en objetos JS
+var bodyParse = require ('body-parser');
+
+//Carga el FW de express
+var app = express();
+
+
+
+
+
+
+
+
+//Exportar
+//Exporta la configuracion
+module.exports = app;
